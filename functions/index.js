f8b8a90dc10a26a54db7eb24307c307e009a4b37
hello-lisai <105758032+hello-lisai@users.noreply.github.com>
2024-03-01 19:51:10 -0800

    Update index.js

--- functions/index.js
+++ functions/index.js
@@ -1,44 +1,11 @@
-const doh = 'https://security.cloudflare-dns.com/dns-query'
-const dohjson = 'https://security.cloudflare-dns.com/dns-query'
-const contype = 'application/dns-message'
-const jstontype = 'application/dns-json'
-
-export const onRequestGet = async ({request}) => {
-	 const { method, headers, url } = request 
-	 const searchParams = new URL(url).searchParams
-	 if (searchParams.has('dns')) {
-	 return await fetch(doh + '?dns=' + searchParams.get('dns'), {
-            method: 'GET',
-            headers: {
-                'Accept': contype,
-            }
-        });
-	 } else if (method== 'GET' && headers.get('Accept')==jstontype) {
-        const search = new URL(url).search
-         return await fetch(dohjson + search, {
-            method: 'GET',
-            headers: {
-                'Accept': jstontype,
-            }
-        });
-    } else {
-        return new Response("", {status: 404})
-    }
-
-}
-
-export const onRequestPost = async ({ request }) => {
-	const { headers } = request 
-  if (headers.get('content-type')==contype) {
-        return fetch(doh, {
-            method: 'POST',
-            headers: {
-                'Accept': contype,
-                'Content-Type': contype,
-            },
-            body: request.body,
-        });
-		 } else {
-        return new Response("", {status: 404})
-    }
-}
+addEventListener(
+  "fetch",event => {
+     let url=new URL(event.request.url);
+     url.hostname="abc.def.xyz"; // 修改成自己的节点IP/域名
+     url.protocol='https'; // 如为http协议请修改为http
+     let request=new Request(url,event.request);
+     event. respondWith(
+       fetch(request)
+     )
+  }
+)
