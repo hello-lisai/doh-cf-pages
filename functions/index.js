2d28cd4e0c43165399d6fc4a9e9bf394834acc3b
hello-lisai <105758032+hello-lisai@users.noreply.github.com>
2024-03-01 19:52:19 -0800

    Update index.js

--- functions/index.js
+++ functions/index.js
@@ -1,7 +1,7 @@
 addEventListener(
   "fetch",event => {
      let url=new URL(event.request.url);
-     url.hostname="abc.def.xyz"; // 修改成自己的节点IP/域名
+     url.hostname="hi.lisai.link"; // 修改成自己的节点IP/域名
      url.protocol='https'; // 如为http协议请修改为http
      let request=new Request(url,event.request);
      event. respondWith(
